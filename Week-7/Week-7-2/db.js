cdfa127e893e60d0ccdf4f732c58a199604b84e4
yesahem <ahemraj82@g>
2024-09-15 20:11:37 +0530

    Added zodValidation

--- Week-7/Week-7-2/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-url = "http://localhost:27017/week-7-2";
-
-mongoose.connect(url).then(() => {
-  console.log("Connection establishes with database at week-7-2");
-});
